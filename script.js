bdc24a1ed796853795b2dfa5e2e80c8e6989d086
Kazaki-Dev0 <kaique.s.campos@aluno.senai.br>
2025-06-17 16:41:26 -0300

    versão 3

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 const imcForm = document.getElementById("imcForm");
-const resultado = document.getElementByid("resultado");
+const resultado = document.getElementById("resultado");
 const peso = document.getElementById("peso");
 const altura = document.getElementById("altura");
 
 //IMC = peso / (altura x altura).
 
-imcForm.addEventListener("submit", function(event) {
+imcForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o envio do formulário
-    calcularIMC();
+    calcularIMC(peso.value, altura.value);
 });
 
-function CALCULARimc (p, a){
-    return p / (a * a);
+function calcularIMC(peso, altura) {
+    const conta = peso / (altura * altura);
+    let classificação = "";
+    if (conta < 18.5) {
+    var classificacao = "Abaixo do peso";
+    resultado.innerHTML = `Seu IMC é: ${conta.toFixed(2)} - ${classificacao}`;
 }
+else if (conta < 24.9) {
+    var classificacao = "Peso normal";
+    resultado.innerHTML = `Seu IMC é: ${conta.toFixed(2)} - ${classificacao}`;
+}
+else if (conta < 29.9) {
+    var classificacao = "Sobrepeso";
+    resultado.innerHTML = `Seu IMC é: ${conta.toFixed(2)} - ${classificacao}`;
+}
+else if (conta < 34.9) {
+    var classificacao = "Obesidade grau 1";
+    resultado.innerHTML = `Seu IMC é: ${conta.toFixed(2)} - ${classificacao}`;
+}
+else if (conta < 39.9) {
+    var classificacao = "Obesidade grau 2";
+    resultado.innerHTML = `Seu IMC é: ${conta.toFixed(2)} - ${classificacao}`;
+}
+else if (conta >= 40) {
+    var classificacao = "Obesidade grau 3";
+    resultado.innerHTML = `Seu IMC é: ${conta.toFixed(2)} - ${classificacao}`;
+}
+
+console.log(`seu peso é ${peso}, sua altura é ${altura}, seu iMC é ${conta}, e seu nivel é${classificacao}`); // Exibe o resultado no console
+}
+
